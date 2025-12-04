908e9ba528883250e54a00cb681aea137ba12dc1
Mohd Sohaib <shoaibqu7714@gmail.com>
2025-12-04 23:13:16 +0530

    production ready app

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,43 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import chatbotRoutes from "./routes/chatbotRoutes.js";
+import path from "path";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 connectDB();
 
+const PORT = process.env.PORT || 8080;
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const _dirname = path.resolve();
 
+// Middleware
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/chatbot", chatbotRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// Serve frontend
+app.use(express.static(path.join(_dirname, "frontend", "dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(_dirname, "frontend", "dist", "index.html"));
+});
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
